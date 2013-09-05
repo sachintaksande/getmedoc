1a0e2b694c355daa8395f4d7c8b69204f8ca5b96
Tejas Garde <tejasgarde@yahoo.co.in>
2013-09-06 02:06:48 +0530

    Adding Search functionality from UI through AngularJs

--- src/main/webapp/assets/js/controllers/index.js
+++ src/main/webapp/assets/js/controllers/index.js
@@ -1,1 +1,24 @@
-function welcome(){}
+angular.module('getmedoc',['ngResource'])
+	.config(function ($routeProvider) {debugger;
+		$routeProvider
+			.when('/',{
+				templateUrl: 'doclist.html',
+				controller: 'SearchDocs'
+			});
+	})
+	.controller('SearchDocs',function($scope,$resource){debugger;
+		var Doctors = $resource('/doctors'); 
+	})
+	.controller('SearchCtrl',function($scope,$resource){
+		$scope.onSearch = function() {debugger;
+			var Doctors = $resource('doctors');
+			$scope.doctorsList = Doctors.get({
+						sortBy: 'name',
+						order:'asc',
+						pageNo:1,
+						pageSize: 50
+					});
+			
+			
+		}
+	});
